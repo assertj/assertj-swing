d2821604e63ab8a6f9c4cb7e39d4baefa3403cb6
croesch <christianroesch@gmx.net>
2014-02-05 23:06:01 +0100

    #4 extract super class for ClickRecorder.

--- assertj-swing/src/test/java/org/fest/swing/test/recorder/ClickRecorder.java
+++ assertj-swing/src/test/java/org/fest/swing/test/recorder/ClickRecorder.java
@@ -14,32 +14,28 @@
  */
 package org.fest.swing.test.recorder;
 
-import static org.fest.assertions.Assertions.assertThat;
-import static org.fest.swing.core.MouseButton.LEFT_BUTTON;
-import static org.fest.swing.core.MouseButton.MIDDLE_BUTTON;
-import static org.fest.swing.core.MouseButton.RIGHT_BUTTON;
-import static org.fest.util.Maps.newHashMap;
 import static org.fest.util.Preconditions.checkNotNull;
 
 import java.awt.Component;
 import java.awt.Container;
-import java.awt.Point;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.util.Map;
+import java.awt.event.MouseListener;
 
 import javax.annotation.Nonnull;
 
-import org.fest.assertions.AssertExtension;
-import org.fest.swing.core.MouseButton;
-
 /**
- * An event listener that records mouse events.
+ * An event listener that records mouse events on a specific component as {@link MouseListener}.
  * 
  * @author Alex Ruiz
  * @author Yvonne Wang
  */
-public class ClickRecorder implements AssertExtension {
+public class ClickRecorder extends AbstractClickRecorder {
+
+  ClickRecorder() {
+    // hide the constructor from the outside
+  }
+
   public static @Nonnull
   ClickRecorder attachTo(@Nonnull Component target) {
     ClickRecorder recorder = new ClickRecorder();
@@ -57,18 +53,6 @@ public class ClickRecorder implements AssertExtension {
     }
   }
 
-  private static final Map<Integer, MouseButton> MOUSE_BUTTON_MAP = newHashMap();
-
-  static {
-    MOUSE_BUTTON_MAP.put(MouseEvent.BUTTON1, LEFT_BUTTON);
-    MOUSE_BUTTON_MAP.put(MouseEvent.BUTTON2, MIDDLE_BUTTON);
-    MOUSE_BUTTON_MAP.put(MouseEvent.BUTTON3, RIGHT_BUTTON);
-  }
-
-  private MouseButton clickedButton;
-  private int clickCount;
-  private Point pointClicked;
-
   private static class ClickListener extends MouseAdapter {
     private final ClickRecorder owner;
 
@@ -78,67 +62,12 @@ public class ClickRecorder implements AssertExtension {
 
     @Override
     public void mousePressed(MouseEvent e) {
-      record(checkNotNull(e));
+      owner.record(checkNotNull(e));
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
-      record(checkNotNull(e));
+      owner.record(checkNotNull(e));
     }
-
-    private void record(@Nonnull MouseEvent e) {
-      owner.clickedButton = MOUSE_BUTTON_MAP.get(e.getButton());
-      owner.clickCount = e.getClickCount();
-      owner.pointClicked = e.getPoint();
-    }
-  }
-
-  public @Nonnull
-  ClickRecorder wasNotClicked() {
-    assertThat(clickedButton).isNull();
-    return this;
-  }
-
-  public @Nonnull
-  ClickRecorder timesClicked(int times) {
-    assertThat(clickCount).isEqualTo(times);
-    return this;
-  }
-
-  public @Nonnull
-  ClickRecorder wasClicked() {
-    return clicked(LEFT_BUTTON).timesClicked(1);
-  }
-
-  public @Nonnull
-  ClickRecorder wasDoubleClicked() {
-    return clicked(LEFT_BUTTON).timesClicked(2);
-  }
-
-  public @Nonnull
-  ClickRecorder wasRightClicked() {
-    return clicked(RIGHT_BUTTON).timesClicked(1);
-  }
-
-  public @Nonnull
-  ClickRecorder clicked(@Nonnull MouseButton button) {
-    return wasClickedWith(button);
-  }
-
-  public @Nonnull
-  ClickRecorder wasClickedWith(@Nonnull MouseButton button) {
-    assertThat(clickedButton).isEqualTo(button);
-    return this;
-  }
-
-  public @Nonnull
-  ClickRecorder clickedAt(@Nonnull Point p) {
-    assertThat(pointClicked).isEqualTo(p);
-    return this;
-  }
-
-  public @Nonnull
-  Point pointClicked() {
-    return pointClicked;
   }
 }
