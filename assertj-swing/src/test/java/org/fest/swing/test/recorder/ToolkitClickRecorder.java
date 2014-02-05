42c2527330bd01c5268ef9143092d1eb056bd6a5
croesch <christianroesch@gmx.net>
2014-02-05 23:09:06 +0100

    #4 add ToolkitClickRecorder to listen for mouse events early when the toolkit awt event listeners are invoked.
    Since some popups are shown and it's tested that the right click has
    happened, there is a need for this ToolkitClickRecorder. Because without
    it, the mouse events may never been recorded.
    
    Normal ClickRecorder is added as a MouseListener. But MouseListeners
    don't run when the event is consumed by the toolkit event listeners. The
    popup is shown by a toolkit event listener that consumes the MouseEvent.
    Therefore the MouseEvent is never noticed by the ClickRecorder.

---
+++ assertj-swing/src/test/java/org/fest/swing/test/recorder/ToolkitClickRecorder.java
@@ -0,0 +1,100 @@
+/*
+ * Created on Sep 21, 2007
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ * 
+ * Copyright @2007-2013 the original author or authors.
+ */
+package org.fest.swing.test.recorder;
+
+import static org.fest.util.Lists.newArrayList;
+import static org.fest.util.Maps.newHashMap;
+import static org.fest.util.Preconditions.checkNotNull;
+
+import java.awt.AWTEvent;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Toolkit;
+import java.awt.event.AWTEventListener;
+import java.awt.event.MouseEvent;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Nonnull;
+
+/**
+ * An event listener that records mouse events on a specific component as {@link AWTEventListener} in the
+ * {@link Toolkit}.
+ * 
+ * @author Christian Rösch
+ */
+public class ToolkitClickRecorder extends AbstractClickRecorder {
+
+  private static Map<ToolkitClickRecorder, ClickListener> recorderListeners = newHashMap();
+
+  ToolkitClickRecorder() {
+    // hide the constructor from the outside
+  }
+
+  static @Nonnull
+  ToolkitClickRecorder attachTo(@Nonnull Component target) {
+    ToolkitClickRecorder recorder = new ToolkitClickRecorder();
+    ClickListener listener = new ClickListener(recorder);
+    recorderListeners.put(recorder, listener);
+
+    Toolkit.getDefaultToolkit().addAWTEventListener(listener, AWTEvent.MOUSE_EVENT_MASK);
+    attach(listener, target);
+    return recorder;
+  }
+
+  static void remove(@Nonnull ToolkitClickRecorder recorder) {
+    ClickListener listener = recorderListeners.remove(recorder);
+    if (listener != null) {
+      Toolkit.getDefaultToolkit().removeAWTEventListener(listener);
+    }
+  }
+
+  private static void attach(@Nonnull final ClickListener listener, @Nonnull final Component target) {
+    listener.addToolkitComponent(target);
+    if (!(target instanceof Container)) {
+      return;
+    }
+    for (Component c : ((Container) target).getComponents()) {
+      attach(listener, checkNotNull(c));
+    }
+  }
+
+  private static class ClickListener implements AWTEventListener {
+    private final ToolkitClickRecorder owner;
+
+    private final List<Component> toolkitComponents = newArrayList();
+
+    ClickListener(@Nonnull ToolkitClickRecorder owner) {
+      this.owner = owner;
+    }
+
+    public void addToolkitComponent(@Nonnull Component target) {
+      toolkitComponents.add(target);
+    }
+
+    @Override
+    public void eventDispatched(@Nonnull AWTEvent evt) {
+      if (toolkitComponents.contains(evt.getSource())) {
+        MouseEvent event = (MouseEvent) evt;
+        switch (event.getID()) {
+        case MouseEvent.MOUSE_PRESSED:
+        case MouseEvent.MOUSE_RELEASED:
+          owner.record(event);
+          break;
+        }
+      }
+    }
+  }
+}
