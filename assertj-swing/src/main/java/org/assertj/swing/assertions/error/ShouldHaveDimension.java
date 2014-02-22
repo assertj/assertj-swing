7b4fa763e1f77b7fadaa7ff41a4c7fe0890e1412
croesch <christianroesch@gmx.net>
2014-02-23 00:25:13 +0100

    #29 add errors for ImageAssert.

---
+++ assertj-swing/src/main/java/org/assertj/swing/assertions/error/ShouldHaveDimension.java
@@ -0,0 +1,45 @@
+/*
+ * Created on Oct 18, 2010
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ * 
+ * Copyright @2010-2011 the original author or authors.
+ */
+package org.assertj.swing.assertions.error;
+
+import java.awt.Dimension;
+
+import org.assertj.core.error.BasicErrorMessageFactory;
+import org.assertj.core.error.ErrorMessageFactory;
+
+/**
+ * Creates an error message indicating that an assertion that verifies that a value have certain dimension failed.
+ * 
+ * @author Alex Ruiz
+ */
+public class ShouldHaveDimension extends BasicErrorMessageFactory {
+
+  /**
+   * Creates a new </code>{@link ShouldHaveDimension}</code>.
+   * 
+   * @param actual the actual value in the failed assertion.
+   * @param actualSize the size of {@code actual}.
+   * @param expectedSize the expected size.
+   * @return the created {@code ErrorMessageFactory}.
+   */
+  public static ErrorMessageFactory shouldHaveDimension(Object actual, Dimension actualSize, Dimension expectedSize) {
+    return new ShouldHaveDimension(actual, actualSize, expectedSize);
+  }
+
+  private ShouldHaveDimension(Object actual, Dimension actualSize, Dimension expectedSize) {
+    super("expected size:<%sx%s> but was:<%sx%s> in:<%s>", expectedSize.width, expectedSize.height, actualSize.width,
+        actualSize.height, actual);
+  }
+}
---
+++ assertj-swing/src/test/java/org/assertj/swing/assertions/error/ShouldHaveDimension_create_Test.java
@@ -0,0 +1,49 @@
+/*
+ * Created on Sep 26, 2010
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ * 
+ * Copyright @2010-2011 the original author or authors.
+ */
+package org.assertj.swing.assertions.error;
+
+import static junit.framework.Assert.assertEquals;
+import static org.assertj.core.util.Lists.newArrayList;
+import static org.assertj.swing.assertions.error.ShouldHaveDimension.shouldHaveDimension;
+
+import java.awt.Dimension;
+
+import org.assertj.core.description.TextDescription;
+import org.assertj.core.error.ErrorMessageFactory;
+import org.assertj.core.presentation.StandardRepresentation;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Tests for <code>{@link ShouldHaveDimension#create(Description)}</code>.
+ * 
+ * @author Alex Ruiz
+ * @author Yvonne Wang
+ */
+public class ShouldHaveDimension_create_Test {
+
+  private ErrorMessageFactory factory;
+
+  @Before
+  public void setUp() {
+    factory = shouldHaveDimension(newArrayList("Luke", "Yoda"), new Dimension(12, 20), new Dimension(42, 15));
+  }
+
+  @Test
+  public void should_create_error_message() {
+    String message = factory.create(new TextDescription("Test"), new StandardRepresentation());
+    assertEquals("[Test] expected size:<42x15> but was:<12x20> in:<[\"Luke\", \"Yoda\"]>", message);
+  }
+}
