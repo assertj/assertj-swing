e5136da7553f27be13e5df4732944d21342776aa
Christian Rösch <christianroesch@gmx.net>
2018-11-26 11:48:59 +0100

    Update parent POM.

--- assertj-swing/src/test/java/org/assertj/swing/core/TestRobots.java
+++ assertj-swing/src/test/java/org/assertj/swing/core/TestRobots.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
  *
@@ -8,14 +8,14 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  *
- * Copyright 2012-2015 the original author or authors.
+ * Copyright 2012-2018 the original author or authors.
  */
 package org.assertj.swing.core;
 
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
 
-import org.mockito.Mockito;
+import org.mockito.ArgumentMatchers;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 
@@ -34,18 +34,17 @@ public final class TestRobots {
   }
 
   public static Robot newRobotMock() {
-    Robot mock = mock(Robot.class);
-    doAnswer(new Answer<Object>() {
+    final Robot mock = mock(Robot.class);
+    doAnswer(new Answer<Object>(){
       @Override
-      public Object answer(InvocationOnMock invocation) throws Throwable {
-        invocation.getArgumentAt(1, Runnable.class).run();
+      public Object answer(final InvocationOnMock invocation) throws Throwable {
+        invocation.<Runnable> getArgument(1).run();
         return null;
       }
-    }).when(mock).pressKeyWhileRunning(Mockito.anyInt(), Mockito.anyObject());
+    }).when(mock).pressKeyWhileRunning(ArgumentMatchers.anyInt(), ArgumentMatchers.anyObject());
 
     return mock;
   }
 
-  private TestRobots() {
-  }
+  private TestRobots() {}
 }
